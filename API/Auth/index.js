2b65cd5393e63bf4ae3f90fc82b0b76f918d9d43
hriday19 <hridaykhurana96@gmail.com>
2021-10-08 01:45:19 +0530

    signin in API added

--- API/Auth/index.js
+++ API/Auth/index.js
@@ -17,22 +17,52 @@ Method        POST
 
 Router.post("/signup", async(req,res) => {
   try {   
-await UserModel.findEmailAndPhone(req.body.credentials);
 
+        await UserModel.findByEmailAndPhone(req.body.credentials);
 
-        //DB
-        await UserModel.create(req.body.credentials);
+         //DB
+              const newUser = await UserModel.create(req.body.credentials);
     
+         //JWT Auth Token
+        const token = newUser.generateJwtToken();
+     //return
+    return res.status(200).json({token, message:"success"});
+     
+     } catch (error) {
+         return res.status(500).json({error: error.message});
+     }
+     });
+ 
 
-    //JWT Auth Token
-    const token = newUser.generateJwtToken();
-//return
-return res.status(200).json({token});
 
-} catch (error) {
-    return res.status(500).json({error: error.message});
-}
-});
+
+/*
+Route         /signin
+Descrip       Signin with email and password
+Params        None
+Access        Public
+Method        POST
+*/
+
+Router.post("/signin", async(req,res) => {
+    try {
+
+
+      const newUser = await UserModel.findByEmailAndPassword(
+        req.body.credentials
+      );
+
+         //JWT Auth Token
+         const token = newUser.generateJwtToken();
+     
+     
+     return res.status(200).json({token , status:"success"});
+     
+     } catch (error) {
+      return res.status(500).json({error: error.message});
+     }
+     });
+          
+      
    
 export default Router;
-
