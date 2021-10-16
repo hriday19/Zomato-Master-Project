3e13ec447e93d3684a3659206a34936a70307c6e
hriday19 <hridaykhurana96@gmail.com>
2021-10-16 16:04:29 +0530

    add restaurant and image reducer

--- API/Image/index.js
+++ API/Image/index.js
@@ -15,6 +15,23 @@ const Router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({storage});
 
+/*
+Route     /
+Des       Get Image details
+Params    _id
+Access    Public
+Method    GET  
+*/
+Router.get("/:_id", async (req, res) => {
+  try {
+    const image = await ImageModel.findById(req.params._id);
+
+    return res.json({ image });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 
 //AWS S3 bucket config
 const s3Bucket = new AWS.S3({
