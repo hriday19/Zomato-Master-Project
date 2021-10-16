0cf3e6b72841e3d6c016c2a19d5d8bb6e9ba9988
hriday19 <hridaykhurana96@gmail.com>
2021-10-16 12:18:51 +0530

    add float menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
 import Temp from "./Components/temp";
 // pages
 import Home from "./Page/Home";
+import Overview from "./Page/Restaurant/Overview";
+import OrderOnline from "./Page/Restaurant/OrderOnline";
 
 function App() {
 
@@ -15,17 +17,19 @@ function App() {
       <Route path="/" exact>
         <Redirect to="/delivery" />
       </Route>
+      <Route path="/restaurant/:id" exact>
+        <Redirect to="/restaurant/:id/overview" />
+      </Route>
       <HomeLayoutHOC path="/:type" exact component={Home} />
-      <RestaurantLayoutHOC path="/restaurant/:id" exact component={Temp} />
       <RestaurantLayoutHOC
         path="/restaurant/:id/overview"
         exact
-        component={Temp}
+        component={Overview}
       />
       <RestaurantLayoutHOC
         path="/restaurant/:id/order-online"
         exact
-        component={Temp}
+        component={OrderOnline}
       />
       <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
       <RestaurantLayoutHOC
@@ -33,6 +37,7 @@ function App() {
         exact
         component={Temp}
       />
+    
       <RestaurantLayoutHOC
         path="/restaurant/:id/photos"
         exact
